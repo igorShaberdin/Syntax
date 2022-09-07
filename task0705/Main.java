100544a4eb4a0ed6ebadf1861010ea110f47882f
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-09-07 13:02:32 +0300

    add 08 task

--- task0705/Main.java
+++ task0705/Main.java
@@ -7,20 +7,20 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-        int[] array = new int[20];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.parseInt(console.readLine());
-        }
-        int[] array1 = new int[10];
-        int[] array2 = new int[10];
-        System.arraycopy(array, 0, array1, 0, array1.length);
-        for (int i = 0; i < array2.length; i++) {
-            array2[i] = array[i + array2.length];
-            System.out.println(array2[i]);
-        }
-        Address howMuch = new Address();
-        howMuch.streetsAndHouses();
+//        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+//        int[] array = new int[20];
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = Integer.parseInt(console.readLine());
+//        }
+//        int[] array1 = new int[10];
+//        int[] array2 = new int[10];
+//        System.arraycopy(array, 0, array1, 0, array1.length);
+//        for (int i = 0; i < array2.length; i++) {
+//            array2[i] = array[i + array2.length];
+//            System.out.println(array2[i]);
+//        }
+//        Address howMuch = new Address();
+//        howMuch.streetsAndHouses();
 
         ArrayList<String> line = new ArrayList<>();
         line.add("don't touch my straps");
@@ -29,9 +29,15 @@ public class Main {
         line.add("please would be nice");
         line.add("you have a problem?");
         System.out.println(line.size());
-        for(String i : line) {
-            System.out.println(i);
+        int largest = line.indexOf(0);
+        int index = 0;
+        for(int i = 0; i < line.size(); i++) {
+            if (line.get(i).length() > largest) {
+                largest = line.get(i).length();
+                index = i;
+            }
         }
-
+            System.out.printf("index: %s, size: %s, line: %s", index, largest, line.get(index));
+        //0708
     }
 }
