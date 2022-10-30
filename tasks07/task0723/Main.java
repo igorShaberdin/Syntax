365a79ea9d145cf332fbe973b88abb21295096ae
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-10-30 20:18:25 +0300

    add 0723 task

---
+++ tasks07/task0723/Main.java
@@ -0,0 +1,11 @@
+package tasks07.task0723;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 30; i >= 0; i--) {
+            System.out.println(i);
+            Thread.sleep(100);
+        }
+        System.out.println("Bom!");
+    }
+}
