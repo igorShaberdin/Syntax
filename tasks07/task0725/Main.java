b3952dd66abe0e7ab6f7598972312190db2e562b
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-11-02 11:01:26 +0300

    add 0725 task

---
+++ tasks07/task0725/Main.java
@@ -0,0 +1,14 @@
+package tasks07.task0725;
+
+public class Main {
+    public static final int A = 5;
+    public final static int B = 2;
+    public final static int C = A * B;
+    public static int getValue() {
+        return C;
+    }
+    public static void main(String[] args) {
+        System.out.println(getValue());
+    }
+
+}
