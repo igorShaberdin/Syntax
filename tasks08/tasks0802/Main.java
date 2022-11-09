8976db880a2e68e4df5d1dc53d479e7f6ea692b0
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-11-09 17:22:19 +0300

    add 0802 task

--- tasks08/tasks0802/Main.java
+++ tasks08/tasks0802/Main.java
@@ -1,4 +1,22 @@
 package tasks08.tasks0802;
 
+import java.util.HashSet;
+
 public class Main {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("арбуз - ягода");
+        set.add("банан - трава");
+        set.add("вишня - ягода");
+        set.add("груша - фрукт");
+        set.add("дыня - овощ");
+        set.add("ежевика - куст");
+        set.add("жень-шень - корень");
+        set.add("земляника - ягода");
+        set.add("ирис - цветок");
+        set.add("картофель - клубень");
+        for(String s : set) {
+            System.out.println(s);
+        }
+    }
 }
