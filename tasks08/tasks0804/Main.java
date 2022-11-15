1d1928625dc7189a9c223861cd22befc5f6e9133
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-11-15 12:48:09 +0300

    add 0804 task

---
+++ tasks08/tasks0804/Main.java
@@ -0,0 +1,25 @@
+package tasks08.tasks0804;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] array = new String[]{"dogs", "hex", "mix", "cats", "fox",
+                "drinks", "rocky"};
+        HashMap<String, Integer> map = addToMap(array);
+        printMap(map);
+    }
+    private static HashMap<String, Integer> addToMap(String[] array) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            map.put(array[i], i);
+        }
+        return map;
+    }
+    private static void printMap(HashMap<String, Integer> map) {
+        for(String s : map.keySet()) {
+            System.out.println(s);
+        }
+    }
+}
