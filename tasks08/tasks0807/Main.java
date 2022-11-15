0b41b5345334fe9e249b5cb5c69524a82b27764d
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-11-15 16:54:56 +0300

    add 0807 task

---
+++ tasks08/tasks0807/Main.java
@@ -0,0 +1,11 @@
+package tasks08.tasks0807;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<?> list = new ArrayList<>();
+        LinkedList<?> listL = new LinkedList<>();
+    }
+}
