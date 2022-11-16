10bd915e55d51c569e14d1d138e74837adcecd6f
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-11-16 16:38:24 +0300

    add 0811 task

---
+++ tasks08/tasks0811/Main.java
@@ -0,0 +1,22 @@
+package tasks08.tasks0811;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    public static List getForList() {
+        return new ArrayList();
+    }
+    public static List setForList() {
+        return new ArrayList();
+    }
+    public static List insertList() {
+        return new LinkedList();
+    }
+    public static List removeList() {
+        return new LinkedList();
+    }
+}
