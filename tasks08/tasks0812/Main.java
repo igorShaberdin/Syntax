b1221f8bd46477f8bdb6c2c4b67ed15eb459ff63
igorShaberdin <shaberdin.exceedteam@gmail.com>
2022-11-17 11:21:27 +0300

    add 0812 task

--- tasks08/tasks0812/Main.java
+++ tasks08/tasks0812/Main.java
@@ -5,23 +5,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         LinkedList<Integer> list = new LinkedList<>();
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 10; i++) {
-            list.add(i, Integer.parseInt(console.readLine()));
+            list.add(Integer.parseInt(console.readLine()));
         }
+        System.out.println(theSame(list));
+    }
+
+    public static String theSame(List list) {
         int max = Integer.MIN_VALUE;
-        for (Integer i : list) {
+        Object number = null;
+        for (int i = 0; i < list.size() - 1; i++) {
             int a = Collections.frequency(list, list.get(i));
             if (a > max) {
                 max = a;
             }
-            else if (max == a) {
-                System.out.println(max + " is " + list.get(i));
+            if (max == a) {
+                number = list.get(i);
             }
         }
+        return max + "times is number " + number;
     }
 }
